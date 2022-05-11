3fc86a548914c0a13183f6ab167aab5f95624a9f
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2022-05-11 15:07:08 +0900

    minor bug fixes

--- utils.py
+++ utils.py
@@ -4,12 +4,13 @@ from datetime import datetime
 def get_match_times(team):
     fixtures = pd.read_csv('epl_fixtures.csv')
     team_fixtures = fixtures.loc[(fixtures['Home Team'] == team) | (fixtures['Away Team'] == team)]
-    return list(team_fixtures['Date'].apply(lambda x: datetime.strptime(x, '%d/%m/%Y %H:%M')))
+    match_times = [datetime.strptime(x, '%d/%m/%Y %H:%M') for x in team_fixtures['Date']]
+    return match_times
 
 def is_valid_post(post, match_times):
-    post_time = datetime.strptime(post["time"], '%d/%m/%Y %H:%M')
+    post_time = post["time"]
 
     for time in match_times:
-        if abs((post_time - time).seconds()) <= 1440:
+        if abs((post_time - time).total_seconds()) <= 86400:
             return True
     return False
