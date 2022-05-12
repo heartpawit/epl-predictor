c86dc54d4caf1039bd7d1af16c1a4bcfd82d8b34
heartpawit <72956704+heartpawit@users.noreply.github.com>
2022-05-12 14:21:43 +0900

    scrape with postids

---
+++ scrape2.py
@@ -0,0 +1,30 @@
+from facebook_scraper import get_posts
+import csv
+import os
+import json
+
+options = {
+    # "progress": True,
+    "comments": True,
+    "allow_extra_requests": False,
+    "reactors": True,
+    "posts_per_page": 200,
+}
+
+def main():
+    with open('epl_fixtures-post_ids.csv', mode='r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        matches = list(csvreader)
+        home_postIDs = [match['Home_PostID'] for match in matches if match['Home_PostID']]
+        away_postIDs = [match['Away_PostID'] for match in matches if match['Away_PostID']]
+        postIDs = home_postIDs + away_postIDs
+        
+        for post in get_posts(post_urls=postIDs, timeout=300, options=options):
+            file_dir = os.path.join('data2', post["post_id"] + '.json')
+            with open(file_dir, 'w') as fp:
+                json.dump(post, fp, default=str, indent=4)
+                print(f'{post["post_id"]}.json saved')
+    
+    
+if __name__ == "__main__":
+    main()
