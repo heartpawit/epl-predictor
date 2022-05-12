4b1ead8bff1cf775a3ae665f5d972e13ba7edec1
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2022-05-12 16:38:13 +0900

    fix bugs

--- scrape2.py
+++ scrape2.py
@@ -6,21 +6,26 @@ import json
 options = {
     # "progress": True,
     "comments": True,
-    "allow_extra_requests": False,
+    "allow_extra_requests": True,
     "reactors": True,
-    "posts_per_page": 200,
+    # "posts_per_page": 200,
 }
 
 def main():
+    save_dir = 'posts'
+
     with open('epl_fixtures-post_ids.csv', mode='r') as csvfile:
         csvreader = csv.DictReader(csvfile)
         matches = list(csvreader)
         home_postIDs = [match['Home_PostID'] for match in matches if match['Home_PostID']]
         away_postIDs = [match['Away_PostID'] for match in matches if match['Away_PostID']]
         postIDs = home_postIDs + away_postIDs
+
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
         
         for post in get_posts(post_urls=postIDs, timeout=300, options=options):
-            file_dir = os.path.join('data2', post["post_id"] + '.json')
+            file_dir = os.path.join(save_dir, post["post_id"] + '.json')
             with open(file_dir, 'w') as fp:
                 json.dump(post, fp, default=str, indent=4)
                 print(f'{post["post_id"]}.json saved')
