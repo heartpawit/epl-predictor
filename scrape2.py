dc338a8ee8902aad2943670258ba08623b503301
Guntitat Sawadwuthikul <guntitat_sawadwuthikul@hotmail.com>
2022-06-01 11:27:45 +0900

    skip exception to continue scraping

--- scrape2.py
+++ scrape2.py
@@ -5,7 +5,7 @@ import json
 
 options = {
     # "progress": True,
-    "comments": 100,
+    "comments": 150,
     "allow_extra_requests": False,
     # "reactors": True,
     # "posts_per_page": 200,
@@ -19,17 +19,25 @@ def main():
         matches = list(csvreader)
         home_postIDs = [match['Home_PostID'] for match in matches if match['Home_PostID']]
         away_postIDs = [match['Away_PostID'] for match in matches if match['Away_PostID']]
-        postIDs = home_postIDs + away_postIDs
+        postIDs = set(home_postIDs + away_postIDs)
 
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
+
+        scraped_files = set([os.path.splitext(path)[0] for path in os.listdir(save_dir)])
+        postIDs -= scraped_files
+        print(f"Attempting to scrape {len(postIDs)} files")
         
-        for post in get_posts(post_urls=postIDs, cookies='facebook.com_cookies.txt', timeout=300, options=options):
+        for post in get_posts(post_urls=postIDs, timeout=300, options=options):
+        # for post in get_posts(post_urls=postIDs, cookies='facebook.com_cookies.txt', timeout=300, options=options):
             file_dir = os.path.join(save_dir, post["original_request_url"] + '.json')
-            with open(file_dir, 'w') as fp:
-                json.dump(post, fp, default=str, indent=4)
-                print(f'{post["original_request_url"]}.json saved')
-    
+            if 'text' in post.keys():
+                with open(file_dir, 'w') as fp:
+                    json.dump(post, fp, default=str, indent=4)
+                    print(f'{post["original_request_url"]}.json saved')
+            else:
+                print(f"Skipping {post['original_request_url']}")
+
     
 if __name__ == "__main__":
     main()
